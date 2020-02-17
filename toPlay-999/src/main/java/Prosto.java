fdb60d517b827339ee93ba04bbf5b4f7e1db5304
vatamanenco <valentin.atamanenco@endava.com>
2020-02-17 16:00:08 +0200

    Implemented WebDriverManager, created test.

--- toPlay-999/src/main/java/Prosto.java
+++ toPlay-999/src/main/java/Prosto.java
@@ -1,21 +1,27 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import net.serenitybdd.screenplay.*;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
 
 public class Prosto {
+
     @Test
-    public void openWebSite(){
-        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+    public void openWebSite() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        Actor sam = Actor.named("Sam");
+        sam.can();
+        TestDriver.createDriver();
 
-        driver.get("https://999.md/ru/");
-        driver.manage().window().maximize();
-
-     WebElement webElement ;
-    // driver.findElements(By.cssSelector("body.page.page-index:nth-child(2) div.wrapper:nth-child(6) div.g-wrap.cf:nth-child(2) div.g-wrap section.categoryPage:nth-child(3) div.categoryContent.cf div.category__subCategories ul.column:nth-child(1) li.category__subCategories-group:nth-child(1) > header:nth-child(1)"));
-   // webElement.findElement(By.cssSelector("body.page.page-index:nth-child(2) div.wrapper:nth-child(6) div.g-wrap.cf:nth-child(2) div.g-wrap section.categoryPage:nth-child(3) div.categoryContent.cf div.category__subCategories ul.column:nth-child(1) li.category__subCategories-group:nth-child(1) > header:nth-child(1)"));
-
+        Thread.sleep(500);
+        WebElement webElement = TestDriver.getDriver().findElement(By.xpath("//div[contains(@class,'g-wrap cf')]//li[1]//a[1]"));
+        webElement.click();
+        TestDriver.closeDriver();
     }
+
+    // driver.findElements(By.cssSelector("body.page.page-index:nth-child(2) div.wrapper:nth-child(6) div.g-wrap.cf:nth-child(2) div.g-wrap section.categoryPage:nth-child(3) div.categoryContent.cf div.category__subCategories ul.column:nth-child(1) li.category__subCategories-group:nth-child(1) > header:nth-child(1)"));
+    // webElement.findElement(By.cssSelector("body.page.page-index:nth-child(2) div.wrapper:nth-child(6) div.g-wrap.cf:nth-child(2) div.g-wrap section.categoryPage:nth-child(3) div.categoryContent.cf div.category__subCategories ul.column:nth-child(1) li.category__subCategories-group:nth-child(1) > header:nth-child(1)"));
 }
